01b528b92dd45d63846e2a363540c52d3502cfa1
Riccardo Coco <riccardoc@USB-debian>
2024-07-23 11:16:59 +0200

    Finished log functionality

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
-	"io/ioutil"
 	"fmt"
+	"log"
 	"net/http"
+	"net/http/httputil"
+	"os"
+	"time"
 )
 
 func Home(w http.ResponseWriter) {
@@ -11,10 +14,30 @@ func Home(w http.ResponseWriter) {
 }
 
 func Help(w http.ResponseWriter) {
-	fmt.Fprintf(w, "Welcome to the help page\n
-				Go to /send to send data that will be saved") // still not finished
+	fmt.Fprintf(w, "Welcome to the help page\nGo to /send to send data that will be saved")
 }
 
 func Save(w http.ResponseWriter, r *http.Request, filePath string) {
-	
+	file, err := os.OpenFile("logged.rbgc", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Serialize the request to a byte slice
+	dumpedReq, err := httputil.DumpRequestOut(r, true)
+	if err != nil {
+		log.Printf("Failed to dump request: %v", err)
+		log.Fatal(err)
+	}
+
+	// Convert the byte slice to a string
+	ip := string(dumpedReq)
+
+	time := time.Now().Format("2006-01-02 15:04:05") // YYYY-MM-DD HH-MM-SS
+
+	var text string = time + ip + "\n"
+
+	if _, err := file.WriteString(text); err != nil {
+		log.Fatal(err)
+	}
 }
