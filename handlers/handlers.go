d8f379c52bd6260d5c5bcff5959344e6c0871b4f
Riccardo Coco <riccardoc@USB-debian>
2024-07-23 10:58:37 +0200

    Started working on logging functionality

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,15 +1,20 @@
 package handlers
 
 import (
+	"io/ioutil"
 	"fmt"
 	"net/http"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
+func Home(w http.ResponseWriter) {
 	fmt.Fprintf(w, "Welcome to the homepage, go to /help for more information")
 }
 
-func Help(w http.ResponseWriter, r *http.Request) {
+func Help(w http.ResponseWriter) {
 	fmt.Fprintf(w, "Welcome to the help page\n
 				Go to /send to send data that will be saved") // still not finished
+}
+
+func Save(w http.ResponseWriter, r *http.Request, filePath string) {
+	
 }
